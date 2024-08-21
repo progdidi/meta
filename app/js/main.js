ae6c7d1b65759c235059c65b3610970508a7e81a
Diana Sosnovskaya <90276229+progdidi@users.noreply.github.com>
2024-08-21 17:59:49 +0300

    Add files via upload

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+//times
+
+const timer = document.querySelector(".timer");
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+const startBtn = timer.querySelector(".start");
+const pauseBtn = timer.querySelector(".pause");
+const stopBtn = timer.querySelector(".stop");
+
+function getRemaingTime() {}
